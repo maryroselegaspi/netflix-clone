99cfd62be066a437a944f8b159c0aaf639b7a7cd
Mary <maryrose.legaspi2018@gmail.com>
2020-10-05 19:55:22 +1000

    Adding  movies

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import request from './request.js';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import request from "./request.js";
 
 function App() {
   return (
     <div className="app">
       <h1>Building netflix clone</h1>
-      <Row title='Trending Now' fetchUrl={request.fetchTrending}/>
-      <Row title="Originals"  fetchUrl={request.fetchNetflixOriginal}/>
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Originals" fetchUrl={request.fetchNetflixOriginal} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Romance" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHOrrorMovies} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -26,6 +26,7 @@ function Row({ title, fetchURL }) {
         {/* several row posters */}
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
