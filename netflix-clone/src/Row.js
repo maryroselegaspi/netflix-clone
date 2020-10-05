7ea633fffe98414477bdc41e9c6defc7d7c9d4db
Mary <maryrose.legaspi2018@gmail.com>
2020-10-05 19:45:40 +1000

    row.css update

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,39 +1,41 @@
-import React,{useState, useEffect} from 'react';
-import './Row.css';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import "./Row.css";
+import axios from "./axios";
 
-const base_url='https://image.tmdb/org/t/p/original/'
-function Row({title,fetchURL}) {
-    const [movies, setMovies] = useState([]);
+const base_url = "https://image.tmdb/org/t/p/original/";
+function Row({ title, fetchURL }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(()=>{
-        // if [] run once when the row loads, and dont run again
-        async function fetchData() {
-            const request =  await axios.get(fetchURL);
-            console.log("this is the request:", request.data.results);
-            setMovies();
-            return request;
-        }
-        fetchData();
+  useEffect(() => {
+    // if [] run once when the row loads, and dont run again
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      console.log("this is the request:", request.data.results);
+      setMovies();
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
 
-    },[fetchURL]);
+  console.log(movies);
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      {/* Title */}
+      <div className="row__posters">
+        {/* several row posters */}
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
 
-    console.log(movies);
-    return (
-        <div className='row'>      
-            <h1>{title}</h1>
-            {/* Title */}
-            <div className='row__posters'>
-                {/* several row posters */}
-                {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
-                ))}
-            </div>
-
-            {/* Container => posters */}
-            
-        </div>
-    )
+      {/* Container => posters */}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
