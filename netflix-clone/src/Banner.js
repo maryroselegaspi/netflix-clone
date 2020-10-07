f45a67b01d5fc0fe30906123348e7a9e89dd05d8
Mary <maryrose.legaspi2018@gmail.com>
2020-10-07 18:36:14 +1000

    banner button added

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -3,6 +3,8 @@ import "./Banner.css";
 import axios from "./axios";
 import requests from "./request.js";
 
+const base_url = "https://image.tmdb.org/t/p/original/"; // for image
+
 function Banner() {
   const [movie, setMovie] = useState([]);
 
@@ -15,23 +17,42 @@ function Banner() {
           Math.floor(Math.random() * request.data.results.length - 1)
         ]
       );
-      console.log(
-        request.data.results[
-          Math.floor(Math.random() * request.data.results.length - 1)
-        ]
-      );
+      // console.log(
+      //   "banner fetch",
+      //   request.data.results[
+      //     Math.floor(Math.random() * request.data.results.length - 1)
+      //   ]
+      // );
 
       return request;
       // randomly select one
     }
     fetchData();
   }, []);
+
+  console.log("Banner fetch", movie);
+  console.log("path:", movie.backdrop_path);
   return (
-    <div className="banner">
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backGroundImage: `('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
+        // backGroundImage: `url("${base_url}${movie.poster_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
       <header>
-        {/* title */}
-        {/* div 2 buttons */}
-        {/* description */}
+        <div className="banner__contents">
+          {/* title */}
+          <h1>{movie?.original_name || movie?.title || movie?.name}</h1>
+          {/* div 2 buttons */}
+          <div className="banner__buttons">
+            <button className="banner_button">Play</button>
+            <button className="banner_button">My List</button>
+          </div>
+          {/* description */}
+        </div>
       </header>
     </div>
   );
