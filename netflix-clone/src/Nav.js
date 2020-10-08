fa50b512224671798630635fd80e035caf2592b5
Mary <maryrose.legaspi2018@gmail.com>
2020-10-08 18:05:24 +1000

    nav icons added

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./Nav.css";
 import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Navbar() {
   const [show, handleShow] = useState(false);
@@ -22,23 +25,18 @@ function Navbar() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
         alt="Netflix logo"
       />
-      {/* <img
-        className="nav__giftbox"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQm_exyapndffeMTDqMe9QexEruiMEgMe7Cbg&usqp=CAU"
-        alt="gift
-        box"
-      /> */}
-      {/* <div className="nav__icons"> */}
-      {/* <CardGiftcardIcon
-          clasname="nav__giftcard"
-          style={{ color: "white", fontSize: 90 }}
-          // fontSize="large"
-        /> */}
-      <img
-        className="nav__avatar"
-        src="https://ih1.redbubble.net/image.618427277.3222/flat,800x800,075,f.u2.jpg"
-        alt=""
-      />
+      <div className="nav__icons">
+        <SearchIcon className="nav__icon" fontSize="large" />
+        <CardGiftcardIcon className="nav__icon" fontSize="large" />
+        <NotificationsIcon className="nav__icon" fontSize="large" />
+        <img
+          className="nav__avatar"
+          src="https://ih1.redbubble.net/image.618427277.3222/flat,800x800,075,f.u2.jpg"
+          alt=""
+        />
+        <ArrowDropDownIcon className="nav__icon" fontSize="large" />
+      </div>
+
       {/* </div> */}
     </div>
   );
