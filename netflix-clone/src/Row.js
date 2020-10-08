e6e35ea47c847317005a4fd996cd06a9de8cc706
Mary <maryrose.legaspi2018@gmail.com>
2020-10-08 13:35:07 +1000

    add trailer url

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./Row.css";
 import axios from "./axios";
-// import axios from "axios";
-// const baseURL: "https://api.themoviedb.org/3/";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+import "url-search-params-polyfill";
 
 const APIKEY = "5a7c7a0a33463163ae0c76e66fa4029a"; //api_key
 
@@ -10,23 +11,38 @@ const base_url = "https://image.tmdb.org/t/p/original/"; // for image
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     // if [] run once when the row loads, and dont run again
     async function fetchData() {
-      const request = await axios.get(
-        fetchUrl
-        // "https://api.themoviedb.org/3/" + fetchURL
-        // `https://api.themoviedb.org/3/discover/movie?api_key=${APIKEY}&with_genres=28`
-      );
-      // console.log("this is the request:", request.data.results);
+      const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchUrl]);
 
-  // console.log("movies:", movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
   return (
     <div className="row">
       <h1>{title}</h1>
@@ -36,6 +52,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -48,6 +65,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
       </div>
 
       {/* Container => posters */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
